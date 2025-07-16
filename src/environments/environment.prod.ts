e5599851cbba2862e8e1fab14bbc9974c4d3fd49
grGrimales <grojasgrimales@gmail.com>
2025-07-16 16:51:45 -0300

    preparar produccion

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    backendUrl: 'https://idiomas-app-bk.vercel.app',
+    cloudinary: {
+      cloudName: 'tu_cloud_name_produccion', // <-- Reemplaza con tu Cloud Name de producción
+      uploadPreset: 'tu_upload_preset_produccion' // <-- Reemplaza con tu Upload Preset de producción
+    }
+  };
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    backendUrl: 'http://localhost:3000',
+    cloudinary: {
+      cloudName: 'dcxto1nnlasdaa', // <-- Reemplaza con tu Cloud Name de desarrollo
+      uploadPreset: 'angular_audio_upls--dsada' // <-- Reemplaza con tu Upload Preset de desarrollo
+    }
+  };
