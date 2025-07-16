9de737b7a43ee292e951a1c76d2955ab7b7ccada
grGrimales <grojasgrimales@gmail.com>
2025-07-16 18:34:24 -0300

    preparar produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://localhost:3000',
+    backendUrl: 'https://idiomas-app-bk.vercel.app',
     cloudinary: {
       cloudName: 'dcxto1nnlasdaa', // <-- Reemplaza con tu Cloud Name de desarrollo
       uploadPreset: 'angular_audio_upls--dsada' // <-- Reemplaza con tu Upload Preset de desarrollo
